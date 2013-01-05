fef2edc2a77cab104d91bc5a0b55e872c6eca3cd
Zearin <zearin@gonk.net>
2013-01-05 13:17:33 -0500

    Added package-level docstring
    Based it on Parsimonius’ `README.rst`.

--- parsimonious/__init__.py
+++ parsimonious/__init__.py
@@ -1,1 +1,19 @@
+"""Parsimonious aims to be the fastest arbitrary-lookahead parser written in 
+pure Python. 
 
+It's based on parsing expression grammars (PEGs), which means you feed it a
+simplified sort of EBNF notation. Parsimonious was designed to undergird a
+MediaWiki parser that wouldn't take 5 seconds or a GB of RAM to do one page.
+
+Goals
+-----
+* Speed
+* Frugal RAM use
+* Minimalistic, understandable, idiomatic Python code
+* Readable grammars
+* Extensible grammars
+* Complete test coverage
+* Good error reporting
+* Separation of concerns
+
+"""
