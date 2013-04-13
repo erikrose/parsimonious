875930a1516f693ea88aa25916f52bb897b15756
Erik Rose <grinch@grinchcentral.com>
2013-04-12 21:26:01 -0700

    Tolerate the unicode flag being on in Python 3 regexes.
    This fixes another failing test.

--- parsimonious/tests/test_grammar.py
+++ parsimonious/tests/test_grammar.py
@@ -1,3 +1,4 @@
+from sys import version_info
 from unittest import TestCase
 
 from nose import SkipTest
@@ -186,8 +187,12 @@ class GrammarTests(TestCase):
                           #Oh yeah.#""")  # Make sure a comment doesn't need a
                                           # \n or \r to end.
         eq_(str(grammar), '''bold_text = stars text stars\n'''
-                          '''text = ~"[A-Z 0-9]*"i\n'''
-                          '''stars = "**"''')
+                          '''text = ~"[A-Z 0-9]*"i%s\n'''
+                          # Unicode flag is on by default in Python 3. I wonder
+                          # if we should turn it on all the time in
+                          # Parsimonious.
+                          '''stars = "**"''' % ('u' if version_info >= (3,)
+                                                else ''))
 
     def test_not(self):
         """Make sure "not" predicates get parsed and work properly."""
