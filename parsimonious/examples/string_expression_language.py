8d975b46c2a6d9dca081389221e37e1ec4153f66
Adam Feuer <adam@adamfeuer.com>
2014-08-16 22:45:05 -0700

    string expression language can evaluate expression strings
    - and basic unit test

--- parsimonious/examples/string_expression_language.py
+++ parsimonious/examples/string_expression_language.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
-from parsimonious import Grammar
+from parsimonious import Grammar, NodeVisitor
 
 
 string_expression_grammar = r"""
@@ -15,12 +15,14 @@ line = expression / ignored_line
 
 begin = whitespace begin_symbol whitespace
 end = whitespace end_symbol whitespace
-expression = whitespace identifier whitespace assignment_symbol whitespace expression_value whitespace newline?
+expression = whitespace variable whitespace assignment_symbol whitespace expression_value whitespace newline?
 
-expression_value = identifier_group / literal_string
-identifier_group = (whitespace identifier)+
+expression_value = literal_string / identifier_group
+identifier_group = (whitespace identifier_value)+
 literal_string = ~"'.*'"
 comment_text = ~".*"
+variable = identifier
+identifier_value = identifier
 identifier = ~"[a-zA-Z0-9_]+"
 single_whitespace = ~"[ \t]"
 whitespace = single_whitespace*
@@ -34,14 +36,70 @@ newline = "\n"
 """
 
 
+class BaseStringExpressionNodeVisitor(NodeVisitor):
+    def __init__(self, identifiers = None):
+        super(BaseStringExpressionNodeVisitor, self).__init__()
+        self.identifiers = identifiers or {}
+
+    def visit_literal_string(self, node, visited_children):
+        return node.text.strip("'")
+
+    def visit_identifier(self, node, visited_children):
+        return node.text
+
+    def visit_variable(self, node, visited_children):
+        return node.text
+
+    def visit_expression_value(self, node, visited_children):
+        result = ''.join(visited_children)
+        return result
+
+    def visit_expression(self, node, visited_children):
+        result = [self.visit(child_node) for child_node in node.children]
+        result = [item for item in result if item]
+        variable_name = result[0]
+        expression_value = result[1]
+        self.identifiers[variable_name] = expression_value
+
+    def generic_visit(self, node, visited_children):
+        pass
+
+
+class IdentifierGroupNodeVisitor(BaseStringExpressionNodeVisitor):
+    def __init__(self, identifiers = None):
+        super(IdentifierGroupNodeVisitor, self).__init__()
+        self.identifiers = identifiers or {}
+
+    def visit_identifier(self, node, visited_children):
+        result = self.identifiers[node.text]
+        return result
+
+
+class StringExpressionNodeVisitor(BaseStringExpressionNodeVisitor):
+    def __init__(self, identifiers = None):
+        super(StringExpressionNodeVisitor, self).__init__()
+        self.identifiers = identifiers or {}
+
+    def visit_identifier_group(self, node, visited_children):
+        identifier_group_node_visitor = IdentifierGroupNodeVisitor(identifiers=self.identifiers)
+        nodes = []
+        for child_node in node.children:
+            nodes += child_node.children
+        values = [identifier_group_node_visitor.visit(node) for node in nodes]
+        values = [value for value in values if value is not None]
+        result = ''.join(values)
+        return result
+
+
 class StringExpressionLanguage(object):
     def __init__(self):
         self._grammar = Grammar(string_expression_grammar)
 
-
     def evaluate(self, text):
-        nodes = self._grammar.parse(text)
-        print nodes
+        root_node = self._grammar.parse(text)
+        node_visitor = StringExpressionNodeVisitor()
+        node_visitor.visit(root_node)
+        return node_visitor.identifiers
 
     def main(self):
         filename = sys.argv[1]
--- parsimonious/examples/string_expression_language_test.py
+++ parsimonious/examples/string_expression_language_test.py
@@ -19,5 +19,14 @@ class StringExpressionLanguageTest(unittest.TestCase):
     def test_expressions(self):
         self.string_expression_language.evaluate("{foo = 'bar'}")
         self.string_expression_language.evaluate("{foo = 'abc' \n baz = 'def'}")
-        self.string_expression_language.evaluate("{foo = 'abc' \n baz = 'def' \n frob = foo baz }")
+
+    def test_expression_group(self):
+        expected_result = { 'foo': 'abc', 'bar': 'xyz', 'baz': 'def', 'frob': 'abcxyzdef'}
+        actual_result = self.string_expression_language.evaluate("{foo = 'abc' \n bar = 'xyz' \n baz = 'def' \n frob = foo bar baz }")
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_evaluation(self):
+        result = self.string_expression_language.evaluate("{foo = 'bar'}")
+        print result
 
