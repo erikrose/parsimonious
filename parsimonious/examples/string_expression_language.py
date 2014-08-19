52a17e1bc8c6bdeb24c86cb26d44e4b35976f71c
Erik Rose <grinch@grinchcentral.com>
2014-08-19 02:01:00 -0400

    Make some minor changes for consistency with the rest of the code.
    * Rewrap docstrings to 79 columns, and reformat them as Sphinx syntax.
    * Move docstring above imports.
    * Indent grammar, and wrap it to 79 columns.
    * Compact a few returns.
    * Remove the unneeded encoding declaration on an empty __init__.

--- parsimonious/examples/string_expression_language.py
+++ parsimonious/examples/string_expression_language.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-from parsimonious import Grammar, NodeVisitor
+"""This is a demonstration of a small domain-specific language for building
+strings up out of other strings.
 
-"""
+You can define variables that are equal to literal strings or built up out of
+other variables.
 
-This is a demonstration of a small domain-specific language
-for building strings up out of other strings.
+Evaluating a program will return a dictionary of all the variables expanded
+into their full strings.
 
-You can define variables that are equal to literal strings or
-built up out of other variables.
-
-Evaluating a program will return a dictionary of all the
-variables expanded into their full strings.
-
-Variables are evaluated in order, so redefining one
-will overwrite the previous definition.
+Variables are evaluated in order, so redefining one will overwrite the previous
+definition.
 
 Comments start with a hash character ('#').
 
-For example:
+For example::
 
     {
         # test program
@@ -30,47 +25,47 @@ For example:
         d = c + a + b
     }
 
-This would return a python dictionary:
+This would return a python dictionary::
 
-{ "a" : "xyz", b: "abc", c: "333", d: "333xyzabc" }
-
+    {"a" : "xyz", b: "abc", c: "333", d: "333xyzabc"}
 
 """
+import sys
+from parsimonious import Grammar, NodeVisitor
 
 
 string_expression_grammar = r"""
+    program = program_body / ignored_line*
+    program_body = ignored_line* begin line* end ignored_line*
+    ignored_line = comment_line / blank_line
+    comment_body = whitespace comment_symbol comment_text
+    comment_line = comment_body newline
+    blank_line = whitespace newline?
+    line = expression / ignored_line
 
-program = program_body / ignored_line*
-program_body = ignored_line* begin line* end ignored_line*
-ignored_line = comment_line / blank_line
-comment_body = whitespace comment_symbol comment_text
-comment_line = comment_body newline
-blank_line = whitespace newline?
-line = expression / ignored_line
+    begin = whitespace begin_symbol whitespace
+    end = whitespace end_symbol whitespace
+    plus = whitespace plus_symbol whitespace
+    expression = whitespace variable whitespace assignment_symbol whitespace
+                 expression_value whitespace newline?
 
-begin = whitespace begin_symbol whitespace
-end = whitespace end_symbol whitespace
-plus = whitespace plus_symbol whitespace
-expression = whitespace variable whitespace assignment_symbol whitespace expression_value whitespace newline?
+    expression_value = literal_string / identifier_group
+    identifier_group = identifier_value (plus identifier_value)*
+    literal_string = ~"'.*'"
+    comment_text = ~".*"
+    variable = identifier
+    identifier_value = identifier
+    identifier = ~"[a-zA-Z0-9_]+"
+    single_whitespace = ~"[ \t]"
+    whitespace = single_whitespace*
 
-expression_value = literal_string / identifier_group
-identifier_group = identifier_value (plus identifier_value)*
-literal_string = ~"'.*'"
-comment_text = ~".*"
-variable = identifier
-identifier_value = identifier
-identifier = ~"[a-zA-Z0-9_]+"
-single_whitespace = ~"[ \t]"
-whitespace = single_whitespace*
-
-begin_symbol = "{"
-end_symbol = "}"
-comment_symbol = "#"
-assignment_symbol = "="
-plus_symbol = "+"
-newline = "\n"
-
-"""
+    begin_symbol = "{"
+    end_symbol = "}"
+    comment_symbol = "#"
+    assignment_symbol = "="
+    plus_symbol = "+"
+    newline = "\n"
+    """
 
 
 class IdentifierGroupNodeVisitor(NodeVisitor):
@@ -79,8 +74,7 @@ class IdentifierGroupNodeVisitor(NodeVisitor):
         self.identifiers = identifiers or {}
 
     def visit_identifier(self, node, visited_children):
-        result = self.identifiers[node.text]
-        return result
+        return self.identifiers[node.text]
 
     def visit(self, node):
         values = self.collect_matching_children(node, "identifier")
@@ -127,8 +121,7 @@ class StringExpressionNodeVisitor(NodeVisitor):
 
     def visit_identifier_group(self, node, visited_children):
         identifier_group_node_visitor = IdentifierGroupNodeVisitor(identifiers=self.identifiers)
-        result = identifier_group_node_visitor.visit(node)
-        return result
+        return identifier_group_node_visitor.visit(node)
 
     def generic_visit(self, node, visited_children):
         pass
