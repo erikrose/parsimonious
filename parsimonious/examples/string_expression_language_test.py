41036a223d27bbb49c4205460b13273faa4c91f2
Erik Rose <grinch@grinchcentral.com>
2014-08-19 02:15:39 -0400

    Use nose, simplifying tests.

--- parsimonious/examples/string_expression_language_test.py
+++ parsimonious/examples/string_expression_language_test.py
@@ -1,49 +1,49 @@
 # -*- coding: utf-8 -*-
-import unittest
+from nose.tools import eq_
+
 from parsimonious.examples.string_expression_language import evaluate
 
 
-class StringExpressionLanguageTest(unittest.TestCase):
-    def test_empty_program(self):
-        evaluate("")
-        evaluate("#X\n")
-        evaluate("{}")
-        evaluate("{#\n}")
-        evaluate("# comment\n")
-        evaluate(" { # comment \n} ")
+def test_empty_program():
+    evaluate("")
+    evaluate("#X\n")
+    evaluate("{}")
+    evaluate("{#\n}")
+    evaluate("# comment\n")
+    evaluate(" { # comment \n} ")
 
-    def test_expressions(self):
-        evaluate("{foo = 'bar'}")
-        evaluate("{foo = 'abc' \n baz = 'def'}")
+def test_expressions():
+    evaluate("{foo = 'bar'}")
+    evaluate("{foo = 'abc' \n baz = 'def'}")
 
-    def test_evaluation(self):
-        expected_result = {'foo': 'bar'}
-        actual_result = evaluate("{foo = 'bar'}")
-        self.assertEqual(expected_result, actual_result)
+def test_evaluation():
+    expected_result = {'foo': 'bar'}
+    actual_result = evaluate("{foo = 'bar'}")
+    eq_(expected_result, actual_result)
 
-    def test_expression_group_evaluation(self):
-        expected_result = {'foo': 'abc', 'bar': 'xyz', 'baz': 'def', 'frob': 'abcxyzdef'}
-        actual_result = evaluate(
-            "    {   \n foo = 'abc' \n bar = 'xyz' \n baz = 'def' \n frob = foo + bar + baz \n }")
-        self.assertEqual(expected_result, actual_result)
+def test_expression_group_evaluation():
+    expected_result = {'foo': 'abc', 'bar': 'xyz', 'baz': 'def', 'frob': 'abcxyzdef'}
+    actual_result = evaluate(
+        "    {   \n foo = 'abc' \n bar = 'xyz' \n baz = 'def' \n frob = foo + bar + baz \n }")
+    eq_(expected_result, actual_result)
 
-    def test_full_program(self):
-        program_text = """
-            {
-                # a test program
-                part1 = 'frog'
-                part2 = 'cat'
-                part3 = 'lizard'
-                part4 = 'fish'
-                part4 = 'dragon'       # overwrites fish
-                part5 = part4 + part2
-                space = ' '
-                animals = part1 + space + part2 + space + part5
-            }
+def test_full_program():
+    program_text = """
+        {
+            # a test program
+            part1 = 'frog'
+            part2 = 'cat'
+            part3 = 'lizard'
+            part4 = 'fish'
+            part4 = 'dragon'       # overwrites fish
+            part5 = part4 + part2
+            space = ' '
+            animals = part1 + space + part2 + space + part5
+        }
 
-        """
-        expected_result = {"part1": "frog", "part2": "cat", "part3": "lizard", "part4": "dragon", "space": " ",
-                            "part5": "dragoncat",
-                            "animals": "frog cat dragoncat"}
-        actual_result = evaluate(program_text)
-        self.assertEqual(expected_result, actual_result)
+    """
+    expected_result = {"part1": "frog", "part2": "cat", "part3": "lizard", "part4": "dragon", "space": " ",
+                        "part5": "dragoncat",
+                        "animals": "frog cat dragoncat"}
+    actual_result = evaluate(program_text)
+    eq_(expected_result, actual_result)
