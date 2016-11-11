360d251e651f90e3be8e3a609973b870df3eed8d
Erik Rose <erik@mozilla.com>
2016-11-11 15:02:28 -0500

    Expose BadGrammar and VisitationError as top-level imports. Fix #101.

--- parsimonious/__init__.py
+++ parsimonious/__init__.py
@@ -4,6 +4,7 @@ Things may move around in modules deeper than this one.
 
 """
 from parsimonious.exceptions import (ParseError, IncompleteParseError,
-                                     VisitationError, UndefinedLabel)
+                                     VisitationError, UndefinedLabel,
+                                     BadGrammar)
 from parsimonious.grammar import Grammar, TokenGrammar
-from parsimonious.nodes import NodeVisitor, rule
+from parsimonious.nodes import NodeVisitor, VisitationError, rule
