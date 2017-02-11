7c2757752db005bbf63d648d75a7d97cf0d64157
Lucas Wiman <lucas.wiman@gmail.com>
2017-02-11 12:46:50 -0800

    Remove direct calls to dict()

--- parsimonious/tests/test_benchmarks.py
+++ parsimonious/tests/test_benchmarks.py
@@ -13,7 +13,7 @@ timeit = partial(timeit, number=500000)
 def test_lists_vs_dicts():
     """See what's faster at int key lookup: dicts or lists."""
     list_time = timeit('item = l[9000]', 'l = [0] * 10000')
-    dict_time = timeit('item = d[9000]', 'd = dict((x, 0) for x in range(10000))')
+    dict_time = timeit('item = d[9000]', 'd = {x: 0 for x in range(10000)}')
 
     # Dicts take about 1.6x as long as lists in Python 2.6 and 2.7.
     ok_(list_time < dict_time, '%s < %s' % (list_time, dict_time))
