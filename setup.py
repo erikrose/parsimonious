6377cd711af9bf25414f8c8534a3c21a4c1796cc
lonnen <chris.lonnen@gmail.com>
2022-03-23 03:53:32 -0700

    add py310

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries',
         'Topic :: Text Processing :: General'],
